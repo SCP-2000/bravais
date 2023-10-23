72fd7aadb3c3487c2266f589dee4456328611e1a
Nick Cao <nickcao@nichi.co>
2023-10-23 16:30:00 -0400

    more choices

--- app/lib/survey.dart
+++ app/lib/survey.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:survey_kit/survey_kit.dart';
 
 class Survey extends StatelessWidget {
   const Survey({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var steps = [
+        InstructionStep(
+            title: 'Your journey starts here',
+            text: 'Have fun with a quick survey',
+            buttonText: 'Start survey',
+        ),
+        QuestionStep(
+            title: 'Your gender',
+            text: 'gender',
+            answerFormat: SingleChoiceAnswerFormat(
+              textChoices: [
+                TextChoice(text: 'Male', value: 'Male'),
+                TextChoice(text: 'Female', value: 'Female'),
+                TextChoice(text: 'Prefer not to answer', value: 'Not'),
+              ]
+            )
+        )
+    ];
+    var task = OrderedTask(
+      id: TaskIdentifier(id: '321'),
+      steps: steps
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('Second Route'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
-        ),
-      ),
+      body: SurveyKit(
+        onResult: (SurveyResult result) {
+          Navigator.pop(context);
+        },
+        task: task,
+      )
     );
   }
 }
